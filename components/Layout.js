f903b17d77a8500e9177bc2d6be348eac87af4e2
Densatho <dsjacinthotre@gmail.com>
2021-09-19 13:13:08 -0300

    Iniciando projeto

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Head from "next/head";
+import { Flex } from "@chakra-ui/react";
+
+export default function Layout(props) {
+  const title = props.title;
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="Nexus" content="Bet for League of Legends games" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <Flex flexDirection="column" pt={14}>
+        {props.children}
+      </Flex>
+    </div>
+  );
+}
