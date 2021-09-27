20c9bb5cf43510cb7e0a5d031f5de1c857f38b4b
Densatho <dsjacinthotre@gmail.com>
2021-09-27 15:51:00 -0300

    dbInit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+const db_init = require("src/database/DB_init");
 
 function MyApp({ Component, pageProps }) {
   return (
