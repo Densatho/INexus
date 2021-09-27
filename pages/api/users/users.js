5e1814b2f99dceacd0533395e84ee71ef7b20384
Densatho <dsjacinthotre@gmail.com>
2021-09-27 15:47:16 -0300

    removendo header

--- pages/api/users/users.js
+++ pages/api/users/users.js
@@ -3,7 +3,6 @@ const UserConnection = require("src/database/DBConnection/userConnection");
 async function users(request, response) {
   try {
     let users = await UserConnection.getAllUsers();
-    response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
     response.json(users);
   } catch (error) {
     console.log(error);
