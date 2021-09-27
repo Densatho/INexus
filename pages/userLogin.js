69e056a1f95268f4cfd3af41bdf74bfe0d37672a
shollzz <lucashazzi@gmail.com>
2021-09-27 09:29:18 -0300

    Links navbar, navbar global via _app.js

---
+++ pages/userLogin.js
@@ -0,0 +1,21 @@
+const { Container } = require("@chakra-ui/layout");
+
+export default function Navbar() {
+  return (
+    <>
+      <Container
+        alignSelf="center"
+        w="450px"
+        h="450px"
+        color="blue"
+        border="2px"
+        textAlign="center"
+        p="2.5"
+        mt="10"
+      >
+        Tela de login
+      </Container>
+      ;
+    </>
+  );
+}
