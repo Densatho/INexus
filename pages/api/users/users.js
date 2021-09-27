a878176ce83fb535afcf4f70a799ff124b4f4de2
Densatho <dsjacinthotre@gmail.com>
2021-09-27 15:45:10 -0300

    error log

--- pages/api/users/users.js
+++ pages/api/users/users.js
@@ -1,11 +1,13 @@
 const UserConnection = require("src/database/DBConnection/userConnection");
 
 async function users(request, response) {
-  let users = await UserConnection.getAllUsers();
-
-  response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
-
-  response.json(users);
+  try {
+    let users = await UserConnection.getAllUsers();
+    response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+    response.json(users);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default users;
