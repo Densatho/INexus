759d796837319a87c80a83a881b6a330a3e054bb
Densatho <dsjacinthotre@gmail.com>
2021-09-27 12:36:06 -0300

    Modelagem do banco

---
+++ database/models/bet.js
@@ -0,0 +1,23 @@
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const { config } = require("../config");
+
+const sequelize = new Sequelize(config);
+
+const databaseConfig = {
+  ODDS: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  BET_VALUE: {
+    type: DataTypes.FLOAT,
+  },
+  GAIN: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+};
+
+class Bet extends Model {}
+Bet.init(databaseConfig, { sequelize, modelName: "BET" });
+
+module.exports.Bet = Bet;
