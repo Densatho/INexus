9f1726f05dda5e932e6b7016390ec284f2cd66d9
Densatho <dsjacinthotre@gmail.com>
2021-09-27 15:29:05 -0300

    setHeader

--- pages/api/users/users.js
+++ pages/api/users/users.js
@@ -2,6 +2,9 @@ const UserConnection = require("src/database/DBConnection/userConnection");
 
 async function users(request, response) {
   let users = await UserConnection.getAllUsers();
+
+  response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+
   response.json(users);
 }
 
