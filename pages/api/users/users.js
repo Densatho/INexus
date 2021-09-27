c1a1a280dc283bb8bff419eece002a9ab4802b26
Densatho <dsjacinthotre@gmail.com>
2021-09-27 16:52:43 -0300

    ajustando algo

--- pages/api/users/users.js
+++ pages/api/users/users.js
@@ -1,14 +1,15 @@
 const UserConnection = require("src/database/DBConnection/userConnection");
 
 export default async function users(req, res) {
-  if (req.method === "PUT") {
-    UserConnection.createUser(
+  if (req.method === "POST") {
+    const result = await UserConnection.createUser(
       req.body.nickname,
       req.body.password,
       req.body.birthday,
       req.body.cpf,
       req.body.email
     );
+    console.log(result);
   }
 
   let users = await UserConnection.getAllUsers();
