a214a19a6ceef18004b3aec7939225cd9d7f718d
Densatho <Densatho@github.com>
2021-10-01 14:26:57 -0300

    Finalizando APIs

--- pages/api/bet/[nickname]/[gameId].js
+++ pages/api/bet/[nickname]/[gameId].js
@@ -4,7 +4,7 @@ const { formatDateWithHour } = require("src/database/formatDate");
 async function getBetByNicknameAndGameId(req, res) {
   let nickname = req.query.nickname;
   let gameId = req.query.gameId;
-  let bet = await betConn.getBetByUserAndGame(nickname, gameId);
+  let bet = await betConn.getBetByUserAndGame(nickname, parseInt(gameId));
 
   if (bet) {
     if (req.method === "PUT") {
@@ -18,7 +18,7 @@ async function getBetByNicknameAndGameId(req, res) {
     res.json(bet);
   } else {
     res.status(500).json({
-      message: "This Team name doesn't exists",
+      message: "This bet doesn't exists",
     });
   }
 }
--- pages/testeAPI.js
+++ pages/testeAPI.js
@@ -3,41 +3,28 @@ import { Box, Button } from "@chakra-ui/react";
 function TesteApi(props) {
   let testeResp;
   async function testeApi() {
-    const url = "/api/users";
+    const url = "/api/bet/add";
 
-    let users = await fetch(url, {
-      method: "GET",
+    // req.body.odd,
+    // req.body.value,
+    // req.body.nickname,
+    // req.body.gameId,
+    // req.body.teamName
+
+    let testeResp = await fetch(url, {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
+      body: JSON.stringify({
+        odd: 1.5,
+        value: 4000,
+        nickname: "Densatho",
+        gameId: 1,
+        teamName: "pain gaming",
+      }),
     });
-
-    let users_update = await users.json();
-    console.log(users_update);
-
-    Object.entries(users_update).forEach(async (user) => {
-      user = user[1];
-      if (user.BALANCE === null) {
-        user.BALANCE = 0;
-      }
-
-      let testeResp = await fetch(`/api/user/${user.NICKNAME}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          nickname: user.NICKNAME,
-          name: user.NAME,
-          balance: user.BALANCE,
-          lastName: user.LASTNAME,
-          email: user.EMAIL,
-          CPF: user.CPF,
-          birthday: user.BIRTHDAY,
-        }),
-      });
-      console.log(await testeResp.json());
-    });
+    console.log(await testeResp.json());
   }
 
   return (
